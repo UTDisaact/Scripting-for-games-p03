8ae24f6e48b32866e19993d19053c6f69a145806
Isaac Torrez <isaac.torrez@utdallas.edu>
2022-04-19 00:04:03 -0500

    Created Grapplegun mechanics

---
+++ Assets/GrapplingGun.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappeable;
+    public Transform gunTip, camera, player;
+    private float maxDistance = 100f;
+    private SpringJoint joint;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+   
+    void Update()
+    { 
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+    }
+
+    void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    void StartGrapple()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast( origin: camera.position, direction: camera.forward, out hit, maxDistance))
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(a: player.position, b: grapplePoint);
+
+            joint.maxDistance = distanceFromPoint = 0.8f;
+            joint.maxDistance = distanceFromPoint = 0.25f;
+
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+
+            lr.positionCount = 2;
+        }
+    }
+
+    void DrawRope()
+    {
+        if (!joint) return;
+        lr.SetPosition(index: 0, gunTip.position);
+        lr.SetPosition(index: 1, grapplePoint);
+    }
+
+    void StopGrapple()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    public bool IsGrappling()
+    {
+        return joint != null;
+    }
+
+    public Vector3 GetGrapplePoint()
+    {
+        return grapplePoint;
+    }
+}
