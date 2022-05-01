931169b06ac1eaf3ca42da03a3cd79d9b511f520
Isaac Torrez <isaac.torrez@utdallas.edu>
2022-05-01 12:37:43 -0500

    Created Obstacle course and added audio

---
+++ Assets/Playonclick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playonclick : MonoBehaviour
+
+{
+
+    public AudioSource someSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            someSound.Play();
+        }
+        
+
+    }
+}
---
+++ Assets/rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    float x;
+    void Update()
+    {
+        x += Time.deltaTime * 10;
+        transform.rotation = Quaternion.Euler(x, 0, 0);
+    }
+}
